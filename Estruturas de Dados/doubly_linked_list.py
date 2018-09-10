39f6ec00ad7a7315f98d284ec7f57e1830b75789
Gabriel Cavalcanti <gabrielnick98@gmail.com>
2018-09-10 10:38:13 -0300

    append, pop, len, empty

--- Estruturas de Dados/doubly_linked_list.py
+++ Estruturas de Dados/doubly_linked_list.py
@@ -11,11 +11,47 @@ class Node():
 		return self.__str__()
 
 class List():
-	def __init__(self, item = None):
+	def __init__(self, *item = None):
+		self.len = 0
 		if item is None:
 			self.head = Node() = self.tail
 		else:
-			pass
+			aux = self.head
+			for x in item:
+				aux.next_node = Node(x,aux)
+				aux = aux.next_node
+				self.len += 1
+			self.tail = aux
+
+	def empty(self):
+		if self.head == self.tail:
+			return True
+		return False
+
+	def __len__(self):
+		return self.len
+
+	def append(self, item):
+		self.tail.next_node = Node(item,self.tail)
+		self.tail = self.tail.next_node
+
+	def pop(self, index):
+		cont = 0
+		aux = self.head.next_node
+		if index >= self.__len__():
+			index = self.__len__() - 1
+		while cont < index:
+			aux = aux.next_node
+			cont+=1
+		if cont == self.__len__():
+			aux.previus_node.next_node = aux.next_node
+			return aux
+		aux.previus_node.next_node = aux.next_node
+		aux.next_node.previus_node = aux.previus_node
+			
+
+
+
 
 
 		
