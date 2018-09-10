84112701ce8de1a405c85bbbd30abc28d7ed56b9
Gabriel Cavalcanti <gabrielnick98@gmail.com>
2018-09-09 23:35:47 -0300

    Node creation

--- Estruturas de Dados/doubly_linked_list.py
+++ Estruturas de Dados/doubly_linked_list.py
@@ -1,3 +1,21 @@
 class Node():
-	def __init__(self,):
+	def __init__(self,item = None, previus_node = None, next_node = None):
+		self.item = item
+		self.previus_node = previus_node
+		self.next_node = next_node
+
+	def __str__(self):
+		return str(self.item)
+
+	def __repr__(self):
+		return self.__str__()
+
+class List():
+	def __init__(self, item = None):
+		if item is None:
+			self.head = Node() = self.tail
+		else:
+			pass
+
+
 		
